4260ccb3c236d7436b378fc998d13b1e348e3149
Dhrubo <insular55@gmail.com>
2020-12-18 21:18:33 +0600

    updated ChildService Test

--- src/test/java/com/project/test/ChildServiceTest.java
+++ src/test/java/com/project/test/ChildServiceTest.java
@@ -2,11 +2,15 @@ package com.project.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 public class ChildServiceTest {
     @Autowired
     private ChildRepository childRepository;
