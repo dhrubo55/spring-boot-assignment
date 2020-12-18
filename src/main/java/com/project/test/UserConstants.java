82054e2d643c14e50f6e83d34df1b290be4e47de
Dhrubo <insular55@gmail.com>
2020-12-18 20:58:45 +0600

    added constants

---
+++ src/main/java/com/project/test/UserConstants.java
@@ -0,0 +1,6 @@
+package com.project.test;
+
+public class UserConstants {
+    public static final String PARENT_NOT_FOUND = "Parent with id %d not found";
+    public static final String CHILD_NOT_FOUND = "Child with id %d not found";
+}
