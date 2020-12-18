453d1905ea6f8101ed9e4ebf53d35e7496b96001
Dhrubo <insular55@gmail.com>
2020-12-18 21:17:31 +0600

    added DirtiesContext to make the context dirty and replace database after each test

--- src/test/java/com/project/test/ParentServiceTest.java
+++ src/test/java/com/project/test/ParentServiceTest.java
@@ -2,14 +2,15 @@ package com.project.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
-import java.util.List;
+import org.springframework.test.annotation.DirtiesContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 public class ParentServiceTest {
     @Autowired
     ParentRepository parentRepository;
